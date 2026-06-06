bb2e1af180418f7b082fe745bcc9a4091a666269
emr09 <emirpdalan@icloud.com>
2026-06-06 11:08:31 +0200

    Initial Commit

---
+++ AsunaTracks/AsunaTracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsunaTracksApp.swift
+//  AsunaTracks
+//
+//  Created by emr09 on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AsunaTracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
