65fea7a2190dd4b7dc4c981892410ff5e351a198
emr09 <emirpdalan@icloud.com>
2026-07-16 21:32:15 +0200

    Added Apple Watch Support and Widgets

---
+++ WatchOS Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchOS Watch App
+//
+//  Created by emr09 on 16/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by emr09 on 16/07/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+        WidgetsControl()
+        WidgetsLiveActivity()
+    }
+}
